8b342c5dfa3610a492386a1991434408d8f30e36
zottaa <zottaa691@gmail.com>
2022-04-05 02:04:55 +0700

    lab1 finished

--- main.py
+++ main.py
@@ -2,23 +2,12 @@ from sympy import *
 import matplotlib.pyplot as plt
 import numpy as np
 from random import randint
-from queue import Queue
+import queue
 
 #8 вариант
 #cos(2t)^2 + sin(2t)
 
 #T поступление (1, 15) T обработки (1, 19)
-
-class Computer:
-    def __init__(self):
-        self.isBusy = False
-
-    def changeCondition(self):
-        if self.isBusy is True:
-            self.isBusy = False
-        else:
-            self.isBusy = True
-
 
 
 def det_dx(f, N):
@@ -26,29 +15,67 @@ def det_dx(f, N):
     t = symbols('t')
     f = lambdify(t, f)
     t = 0
-    det_dx = []
+    _det_dx = []
     count = 0
     while count != N + 1:
-        det_dx.append((f(t + dt) - f(t)) / dt)
+        _det_dx.append((f(t + dt) - f(t)) / dt)
         t += dt
         count += 1
-    return det_dx
+    return _det_dx
 
 
 def model():
-    pass
     N = 0
-    pc_1 = Computer()
-    pc_2 = Computer()
-    t = 0
-    queue = Queue()
+    t_wait_queue = 0
+    t_wait_pc_1 = 0
+    t_wait_pc_2 = 0
+    t_in_system = 0
+    pc_1_is_busy = False
+    pc_2_is_busy = False
+    channel_is_busy = False
+    t_input_end = 0
+    pc_1_end = 0
+    pc_2_end = 0
+    t_input = 0
+    t_handle = 0
+    simulation_time = 0
+    _queue = queue.Queue()
     while N != 1000:
-        Tpost = randint(1, 15)
-        Tobr = randint(1, 19)
-
-
-
-        t += 1
+        if _queue.empty() is False:
+            t_wait_queue += 1
+        if pc_1_is_busy is False:
+            t_wait_pc_1 += 1
+        if pc_2_is_busy is False:
+            t_wait_pc_2 += 1
+        if pc_1_is_busy is True or pc_2_is_busy is True:
+            t_in_system += 1
+        if simulation_time == pc_1_end:
+            N += 1
+            pc_1_is_busy = False
+        if simulation_time == pc_2_end:
+            N += 1
+            pc_2_is_busy = False
+        if pc_1_is_busy is False and _queue.empty() is False:
+            pc_1_end = simulation_time + _queue.get()
+        if pc_2_is_busy is False and _queue.empty() is False:
+            pc_2_end = simulation_time + _queue.get()
+        if channel_is_busy is False:
+            t_input = randint(1, 15)
+            t_handle = randint(1, 19)
+            t_input_end = simulation_time + t_input
+            channel_is_busy = True
+        if simulation_time == t_input_end:
+            channel_is_busy = False
+            if pc_1_is_busy is False:
+                pc_1_end = simulation_time + t_handle
+                pc_1_is_busy = True
+            elif pc_2_is_busy is False:
+                pc_2_end = simulation_time + t_handle
+                pc_2_is_busy = True
+            else:
+                _queue.put(t_handle)
+        simulation_time += 1
+    return t_wait_queue, t_wait_pc_1, t_wait_pc_2, t_in_system
 
 
 
@@ -73,7 +100,25 @@ def main():
     plt.show()
 
     #2
-
+    t_average_wait_queue, t_pc_1_average_p, t_pc_2_average_p, t_average_stay_in_system = 0, 0, 0, 0
+    for i in range(1, 101):
+        print("Эксперимент №%d" % i)
+        t_wait_queue, t_wait_pc_1, t_wait_pc_2, t_in_system = model()
+        t_wait_pc = t_wait_pc_1 + t_wait_pc_2
+        t_average_wait_queue += (t_wait_queue / 1000)
+        t_pc_1_average_p += ((t_wait_pc_1 / t_wait_pc)*100)
+        t_pc_2_average_p += ((t_wait_pc_2 / t_wait_pc)*100)
+        t_average_stay_in_system += (t_in_system / 1000)
+        print("Среднее время пребывания в системе = %.3f сек" % (t_in_system / 1000))
+        print("Среднее время ожидания в очереди = %.3f сек" % (t_wait_queue / 1000))
+        print("Вероятность простоя ПК1 = %.3f" % ((t_wait_pc_1 / t_wait_pc)*100))
+        print("Верояность простоя ПК2 = %.3f" % ((t_wait_pc_2 / t_wait_pc)*100))
+    print("\n" + ("*" * 50) + "\n")
+    print("Результаты после 100 экспериментов")
+    print("Среднее время пребывания в системе = %.3f сек" % (t_average_stay_in_system / 100))
+    print("Среднее время ожидания в очереди = %.3f сек" % (t_average_wait_queue / 100))
+    print("Вероятность простоя ПК1 = %.3f" % (t_pc_1_average_p / 100))
+    print("Вероятность проятоя ПК2 = %.3f" % (t_pc_2_average_p / 100))
 
 
 if __name__ == '__main__':
